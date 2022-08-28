1a4cb3dc6c506c97b72c54bf509bb0091a1a5434
Muwaffaq Imam <muwaffaqimam@gmail.com>
2022-08-28 13:03:03 +0300

    fix issues

--- lib/BlocStateManagmentTemplates/data/UserProvider.dart
+++ lib/BlocStateManagmentTemplates/data/UserProvider.dart
@@ -4,10 +4,12 @@ import 'package:http/http.dart';
 
 class ApiProvider {
   Future<List<User>> getUsers() async {
+    await Future.delayed(Duration(seconds: 2));
+    print("getting User List.....");
     List<User> usersList = [];
     print("connect to internet");
     Response response =
-        await http.get(Uri.parse('https://gorest.co.in/public/v2/users'));
+    await http.get(Uri.parse('https://gorest.co.in/public/v2/users'));
     if (response.statusCode == 200) {
       final body = json.decode(response.body) as List;
       body.forEach((json) {
@@ -18,33 +20,34 @@ class ApiProvider {
     }
     return [];
   }
-}
 
-Future<bool> postUser(
-    {required String name,
-    required String gender,
-    required String email}) async {
-  var response = await http.post(
-      Uri.parse(
-        "https://gorest.co.in/public/v2/users",
-      ),
-      headers: {
-        "Authorization":
-            "Bearer ed0645c09baf75ccb7b21afc0af41ab01f0770fd90e831d5295fab6c77d96965",
-        "Content-Type": "application/json",
-      },
-      body: jsonEncode({
-        "name": "$name",
-        "gender": "$gender",
-        "email": "$email",
-        "status": "active",
-      }));
+  Future<bool> postUser(
+      {required String name,
+        required String gender,
+        required String email}) async {
+    print("Creating User .....");
+    var response = await http.post(
+        Uri.parse(
+          "https://gorest.co.in/public/v2/users",
+        ),
+        headers: {
+          "Authorization":
+          "Bearer ed0645c09baf75ccb7b21afc0af41ab01f0770fd90e831d5295fab6c77d96965",
+          "Content-Type": "application/json",
+        },
+        body: jsonEncode({
+          "name": "$name",
+          "gender": "$gender",
+          "email": "$email",
+          "status": "active",
+        }));
 
-  print(response.body);
-  if (response.statusCode == 201) {
-    return true;
-  } else
-    return false;
+    print(response.body);
+    if (response.statusCode == 201) {
+      return true;
+    } else
+      return false;
+  }
 }
 
 class User {
